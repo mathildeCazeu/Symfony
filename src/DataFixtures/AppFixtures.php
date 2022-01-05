e7ba6fd52d08caf9df44a9179e7077bfd3a7e8a7
Mathilde Cazenave <cazenavemathilde@gmail.com>
2022-01-05 22:56:39 +0100

    Ajout du composant fixture création d'un module de test en BD

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,16 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Formation;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $DUTinformatique = new Formation();
+        $DUTinformatique->setNomCourt("DUT info");
+        $DUTinformatique->setNomLong("Diplome Universitaire Techno informatique");
+        $manager->persist($DUTinformatique);
 
         $manager->flush();
     }
