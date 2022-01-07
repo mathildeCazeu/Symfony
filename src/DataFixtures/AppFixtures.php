dec8a23702e6d93de9c6ea87611c3ed5a19a1f47
Mathilde Cazenave <cazenavemathilde@gmail.com>
2022-01-08 00:31:55 +0100

    Ajout données dans la bd

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,16 +5,45 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Formation;
+use App\Entity\Entreprise;
+use App\Entity\Stage;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $DUTinformatique = new Formation();
-        $DUTinformatique->setNomCourt("DUT info");
-        $DUTinformatique->setNomLong("Diplome Universitaire Techno informatique");
-        $manager->persist($DUTinformatique);
+        //Création d'un générateur de données faker
+        $faker = \Faker\Factory::create('fr_FR');  //Crée des données en fr
 
+        /** ENTREPRISES **/
+        $nbEntreprises = 10;
+
+        for ($i=0; $i <= $nbEntreprises; $i++) 
+        { 
+            $entreprise = new Entreprise();
+            $entreprise->setNom($faker->company);
+            $entreprise->setAdresse($faker->address);
+            $entreprise->setActivite($faker->realText($maxNbChars = 50, $indexSize = 2));
+            $entreprise->setSiteWeb($faker->url);
+
+            $tabEntreprises[] = $entreprise;
+            $manager->persist($entreprise);
+        }
+
+        /** FIN ENTREPRISES **/
+
+        /** STAGES **/
+            
+        /** FIN STAGES **/
+
+        /** FORMATIONS **/
+        $DUT = new Formation();
+        $DUT->setNomCourt("DUT info");
+      //$DUT->setNomLong($faker->realText($maxNbChars = 40, $indexSize = 2));
+        $manager->persist($DUT);
+        /** FIN FORMATIONS **/
+
+        //Envoyer les données en bd
         $manager->flush();
     }
 }
