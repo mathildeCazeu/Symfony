ed053b8019b56bf80611639648264bfa722e1881
Mathilde Cazenave <mcazenave009@iutbayonne.univ-pau.fr>
2022-01-27 09:03:30 +0100

    Ajout user dans la bd - FAIL

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,16 +8,28 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/inscription", name="security_registration")
      */
-    public function registration()
+    public function registration(Request $request, ObjectManager $manager)
     {
         $user = new User();
         $form = $this->createForm(RegistrationType::class, $user);
-        return $this->render('security/registration.html.twig', ['form' => $form->createview()]);
+
+        $form->handleRequest($request); //analyse requete
+
+        if ($form->isSubmitted() && $form->isValid()) //si form soumis et champs valides
+        {
+            $manager->persist($user); //on fait persister l'user dans la bd
+            $manager->flush(); 
+        }
+
+        return $this->render('security/registration.html.twig', 
+        ['form' => $form->createview()] );
     }
 }
